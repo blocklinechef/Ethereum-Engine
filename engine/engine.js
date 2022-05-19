59521fac4e9ef982affd4d0e91e07eeabba34241
blocklinechef <cethereum@protonmail.com>
2022-05-19 21:57:27 +0800

    Initial commit

---
+++ engine/engine.js
@@ -0,0 +1,24 @@
+/* eslint-disable node/no-unpublished-require */
+const ethers = require("ethers");
+const provider = new ethers.providers.WebSocketProvider("ws://localhost");
+const MongoClient = require("mongodb").MongoClient;
+const MongoServerURL = "mongodb://localhost:27017/";
+const client = new MongoClient(MongoServerURL);
+
+const engine = async (txhash) => {
+  await client.connect;
+  provider.getTransaction(txhash).then(function (transaction) {
+    const database = client.db("Engien");
+    const transactions = database.collection("Transactions");
+    if (transaction == null) {
+      return;
+    }
+    const doc = {
+      Hash: transaction.hash,
+      Nonce: transaction.nonce,
+    };
+    transactions.insertOne(doc);
+  });
+};
+
+provider.on("pending", engine);
